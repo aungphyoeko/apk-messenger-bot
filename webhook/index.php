37216d78fb8115467f78989ec4ba38a29777e25e
Aung Phyo Ko <aungphyoeko@user.noreply.github.com>
2017-05-15 06:15:28 -0700

    update

--- webhook/index.php
+++ webhook/index.php
@@ -25,7 +25,7 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 
 
     $sh = curl_init($sender_curl);
-    curl_setopt($sh, CURLOPT_URL, $sender_curl);
+    curl_setopt($sh, CURLOPT_URL);
     curl_setopt($sh, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($sh, CURLOPT_RETURNTRANSFER, true);
     $reply = curl_exec($sh); // user will get the message
