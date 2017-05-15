5c43971783d14ace9d88c1ed82d1dd4b01b42505
Aung Phyo Ko <aungphyoeko@user.noreply.github.com>
2017-05-15 01:35:34 -0700

    update

--- webhook/index.php
+++ webhook/index.php
@@ -25,14 +25,12 @@ if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
     $ch = curl_init($url);
     /*prepare response*/
 
-    $message = GetResponseMessage($message);
-
     $jsonData = '{
     "recipient":{
         "id":"' . $sender . '"
         },
         "message":{
-            "text":"(Bot): ' . $message . '"
+            "text":"(Bot): ' .GetResponseMessage($message ). '"
         }
     }';
     /* curl setting to send a json post data */
