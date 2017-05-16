063eb4c4ce4c2d6e4eab66a21e052552a6e41f24
Aung Phyo Ko <aungphyoeko@user.noreply.github.com>
2017-05-15 21:51:42 -0700

    update

--- webhook/index.php
+++ webhook/index.php
@@ -1,63 +1,55 @@
 <?php
-$messenger = new Messenger();
-$messenger->verify_webhook();
-$messenger->listen_message();
-$messenger->get_name();
-$messenger->send_message();
-class Messenger{
-    protected $PAGE_ACCESS_TOKEN;
-    protected $message;
-    protected $sender_id;
-    protected $sender_name;
-    public function __construct(){
-        $this->PAGE_ACCESS_TOKEN =getenv('PAGE_ACCESS_TOKEN');
-    }
-    public function listen_message(){
-        $input = json_decode(file_get_contents('php://input'), true);
-        if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
 
-            $this->sender_id = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
-            $this->message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
-        }
+/* validate verify token needed for setting up web hook */ 
+if (isset($_GET['hub_verify_token'])) { 
+    if ($_GET['hub_verify_token'] === 'my_secure_verify_token') {
+        echo $_GET['hub_challenge'];
+        return;
+    } else {
+        echo 'Invalid Verify Token';
+        return;
     }
-    public function verify_webhook(){
-    /* validate verify token needed for setting up web hook */ 
-    if (isset($_GET['hub_verify_token'])) { 
-        if ($_GET['hub_verify_token'] === 'my_secure_verify_token') {
-            echo $_GET['hub_challenge'];
-            return;
-        } else {
-            echo 'Invalid Verify Token';
-            return;
-        }
-    }
-    }
-    /* receive and send messages */
+}
 
-public function get_name(){
-    $url = "https://graph.facebook.com/v2.6/$this->sender_id?fields=first_name,last_name&access_token=$this->PAGE_ACCESS_TOKEN";
+$PAGE_ACCESS_TOKEN = getenv('PAGE_ACCESS_TOKEN');
+
+/* receive and send messages */
+$input = json_decode(file_get_contents('php://input'), true);
+if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
+
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
+    $message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
+
+    $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$PAGE_ACCESS_TOKEN";
+    $surl = "https://graph.facebook.com/v2.6/$sender?fields=first_name,last_name&access_token=$PAGE_ACCESS_TOKEN";
+    $name = get_name($surl);
+    send_message($sender,$url,$message,$name);
+    
+}
+function get_name($surl){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $surl);
     $result = curl_exec($ch);
     curl_close($ch);
     $obj = json_decode($result,true);
     curl_close($ch);
-    $this->sender_name = $obj["first_name"];
-    return $this->sender_name;
+    return $obj["first_name"];
 }
-public function send_message(){
+function send_message($sender,$url,$message = '',$name){
+ /*initialize curl*/
+    $ch = curl_init($url);
+    /*prepare response*/
 
     $jsonData = '{
     "recipient":{
-        "id":"' . $this->sender_id . '"
+        "id":"' . $sender . '"
         },
         "message":{
-            "text":"(Bot): Hi '.$this->sender_name.','.$this->message . '"
+            "text":"(Bot): Hi '.$name.','.GetResponseMessage($message ). '"
         }
     }';
-    
     /* curl setting to send a json post data */
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
@@ -66,8 +58,6 @@ public function send_message(){
         $result = curl_exec($ch); // user will get the message
     }
     curl_close($ch);
-}
-
 }
 
 // Processing Messages To Reply
