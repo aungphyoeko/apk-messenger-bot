ebbdd69ed990abd3ca461dd1a012ea02c243913f
Aung Phyo Ko <aungphyoeko@user.noreply.github.com>
2017-05-15 21:47:18 -0700

    update

--- webhook/index.php
+++ webhook/index.php
@@ -2,8 +2,8 @@
 $messenger = new Messenger();
 $messenger->verify_webhook();
 $messenger->listen_message();
-$messenger->send_message($messenger->process_reply_message());
-
+$messenger->get_name();
+$messenger->send_message();
 class Messenger{
     protected $PAGE_ACCESS_TOKEN;
     protected $message;
@@ -11,13 +11,11 @@ class Messenger{
     protected $sender_name;
     public function __construct(){
         $this->PAGE_ACCESS_TOKEN =getenv('PAGE_ACCESS_TOKEN');
-        $this->message = '';
-        $this->sender_id = 0;
-        $this->sender_name= '';
     }
     public function listen_message(){
         $input = json_decode(file_get_contents('php://input'), true);
         if (isset($input['entry'][0]['messaging'][0]['sender']['id'])) {
+
             $this->sender_id = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
             $this->message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
         }
@@ -26,59 +24,55 @@ class Messenger{
     /* validate verify token needed for setting up web hook */ 
     if (isset($_GET['hub_verify_token'])) { 
         if ($_GET['hub_verify_token'] === 'my_secure_verify_token') {
-                echo $_GET['hub_challenge'];
-                return;
-            } 
-            else {
-                echo 'Invalid Verify Token';
-                return;
-            }
+            echo $_GET['hub_challenge'];
+            return;
+        } else {
+            echo 'Invalid Verify Token';
+            return;
         }
     }
-
-    public function get_name(){
-        $url = "https://graph.facebook.com/v2.6/$this->sender_id?fields=first_name,last_name&access_token=$this->PAGE_ACCESS_TOKEN";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        $obj = json_decode($result,true);
-        curl_close($ch);
-        $this->sender_name = $obj["first_name"];
-        return $this->sender_name;
     }
-    public function process_reply_message(){
-        $jsonData = '{
-        "recipient":{
-            "id":"' . $this->sender_id . '"
-            },
-            "message":{
-                "text":"(Bot): You said,'.$this->message . '"
-            }
-        }';
-        return $jsonData;
-    }
+    /* receive and send messages */
 
-    public function send_message($message){
+public function get_name(){
+    $url = "https://graph.facebook.com/v2.6/$this->sender_id?fields=first_name,last_name&access_token=$this->PAGE_ACCESS_TOKEN";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $obj = json_decode($result,true);
+    curl_close($ch);
+    $this->sender_name = $obj["first_name"];
+    return $this->sender_name;
+}
+public function send_message(){
+    $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$this->PAGE_ACCESS_TOKEN";
+ /*initialize curl*/
+    $ch = curl_init($url);
+    /*prepare response*/
 
-        $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$this->PAGE_ACCESS_TOKEN";
-        /*initialize curl*/
-        $ch = curl_init($url);
-        /*prepare response*/
-        /* curl setting to send a json post data */
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        if (!empty($message)) {
-            $result = curl_exec($ch); // user will get the message
+    $jsonData = '{
+    "recipient":{
+        "id":"' . $this->sender_id . '"
+        },
+        "message":{
+            "text":"(Bot): Hi '.$this->sender_name.','.$this->message . '"
         }
-        curl_close($ch);
+    }';
+    /* curl setting to send a json post data */
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    if (!empty($message)) {
+        $result = curl_exec($ch); // user will get the message
     }
+    curl_close($ch);
+}
 
 }
-/**
+
 // Processing Messages To Reply
 $TEAM_DATA;
 function GetResponseMessage($userInput){
@@ -86,5 +80,4 @@ function GetResponseMessage($userInput){
     $TEAM_DATA = json_decode(file_get_contents('teamdata.json'),true);
     return $TEAM_DATA['messages']['greeting'];
 }
-**/
 ?>
