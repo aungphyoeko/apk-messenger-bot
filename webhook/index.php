b5dd3361f1df534c806411b71b8b61eec815b2f3
Aung Phyo Ko <aungphyoeko@user.noreply.github.com>
2017-05-15 22:59:58 -0700

    update

--- webhook/index.php
+++ webhook/index.php
@@ -2,6 +2,7 @@
 $messenger = new Messenger();
 $messenger->verify_token('my_secure_verify_token');
 $messenger->listen_message();
+$messenger->set_reply_message();
 $messenger->process_reply_message();
 $messenger->send_message();
 
@@ -11,12 +12,14 @@ class Messenger{
     protected $sender_id;
     protected $sender_name;
     protected $reply_message;
+    protected $reply_json;
 
     public function __construct(){
         $this->PAGE_ACCESS_TOKEN = getenv('PAGE_ACCESS_TOKEN');
         $this->sender_message = '';
         $this->sender_id = 0;
         $this->sender_name = '';
+        $this->reply_message = '';
     }
     public function verify_token($my_token){
         /* validate verify token needed for setting up web hook */ 
@@ -31,15 +34,20 @@ class Messenger{
         }
     }
     public function process_reply_message(){
+        if($this->reply_message == ''){
+            $this->reply_message == $this->sender_message; // reply back 
+        }
         /*prepare response*/
-        $this->reply_message = '{
+        $this->reply_json = '{
         "recipient":{
             "id":"' . $this->sender_id . '"
             },
             "message":{
-                "text":"(Bot): Hi '.$this->sender_name. ', You said, '.$this->sender_message. '"
+                "text":"(Bot): Hi '.$this->sender_name. ', You said, '.$this->reply_message. '"
             }
         }';
+    }
+    public function set_reply_message($data = ''){
     }
     public function listen_message(){
         $input = json_decode(file_get_contents('php://input'), true);
@@ -47,9 +55,11 @@ class Messenger{
             $this->sender_id = $input['entry'][0]['messaging'][0]['sender']['id']; //sender facebook id
             $this->sender_message = $input['entry'][0]['messaging'][0]['message']['text']; //text that user sent
             $this->sender_name = $this->request_sender_name();
+            $this->reply_message = '';
             return true;
         }
         $this->sender_message = '';
+        $this->reply_message = '';
         return false;
     }
     protected function request_sender_name(){
@@ -60,8 +70,8 @@ class Messenger{
 
     public function send_message(){
         $url = "https://graph.facebook.com/v2.6/me/messages?access_token=$this->PAGE_ACCESS_TOKEN";
-        if ($this->sender_message != ''){
-            $result = $this->curl_send_post_request($url,$this->reply_message);
+        if ($this->reply_message != ''){
+            $result = $this->curl_send_post_request($url,$this->reply_json);
         }
     }
     protected function curl_send_post_request($url,$data){
