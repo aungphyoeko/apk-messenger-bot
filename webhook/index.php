99c1821a98b61428eb34beb68eb775aaedd2e637
Aung Phyo Ko <aungphyoeko@user.noreply.github.com>
2017-09-13 15:58:45 -0700

    update

--- webhook/index.php
+++ webhook/index.php
@@ -25,8 +25,10 @@ class Command{
         $this->KEYWORDS = json_decode(file_get_contents('keywords.json'),true);
         $this->myTeam->read_data_file();
         foreach($this->KEYWORDS as $command=>$keywords){
-            $matches = preg_match_all('/\b('.$keywords.')\b/',strtolower($hear));
-            if($matches > 0) break;
+            foreach ($keywords as $keyword){
+                $matches = preg_match_all('/\b('.$keyword.')\b/',strtolower($hear)); 
+                if($matches > 0) break;  
+            }
             $command = '';
         }
         switch($command){
